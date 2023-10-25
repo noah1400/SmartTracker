3390f0044700ed5e286ef2563c35d1cd9519861f
noah1400 <48360809+noah1400@users.noreply.github.com>
2023-10-25 16:33:35 +0200

    create Auth class + return type def

--- server/backend/smarttracker/app/auth/auth.py
+++ server/backend/smarttracker/app/auth/auth.py
@@ -7,80 +7,89 @@ from flask import request
 from functools import wraps
 import os
 
-services = {
-    'keycloak': {
-        'name': 'keycloak',
-        'config': {
-            'url': os.environ.get('KEYCLOAK_URL'),
-            'realm': os.environ.get('KEYCLOAK_REALM'),
-            'client_id': os.environ.get('KEYCLOAK_CLIENT_ID'),
-            'client_secret': os.environ.get('KEYCLOAK_CLIENT_SECRET')
-        },
-        'service': KeyCloakService,
-    }
-}
-
-def auth_authenticated(f):
-    @wraps(f)
-    def decorated(*args, **kwargs):
-        try:
-            token = request.headers.get('Authorization')
-            if token:
-                token = token.split(' ')[1] # Bearer <token>
-                username = auth_decodeJWT(token)
-                if username:
-                    return f(username, *args, **kwargs)
-        except Exception as e:
-            return { 'error': str(e) }, 401
-        return { 'error': 'Missing Token' }, 401
-    return decorated
 
 
-def auth_generateJWT(username):
-    try:
-        payload = {
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, minutes=30),
-            'iat': datetime.datetime.utcnow(),
-            'sub': username
+class Auth():
+    
+
+    services = {
+        'keycloak': {
+            'name': 'keycloak',
+            'config': {
+                'url': os.environ.get('KEYCLOAK_URL'),
+                'realm': os.environ.get('KEYCLOAK_REALM'),
+                'client_id': os.environ.get('KEYCLOAK_CLIENT_ID'),
+                'client_secret': os.environ.get('KEYCLOAK_CLIENT_SECRET')
+            },
+            'service': KeyCloakService,
         }
-        return jwt.encode(
-            payload,
-            os.environ.get('SECRET_KEY'),
-            algorithm='HS256'
-        )
-    except Exception as e:
-        return e
+    }
+
+    _user = None
+
+    def auth_authenticated(self, f):
+        @wraps(f)
+        def decorated(*args, **kwargs):
+            try:
+                token = request.headers.get('Authorization')
+                if token:
+                    token = token.split(' ')[1] # Bearer <token>
+                    username = self.auth_decodeJWT(token)
+                    if username:
+                        return f(username, *args, **kwargs)
+            except Exception as e:
+                return { 'error': str(e) }, 401
+            return { 'error': 'Missing Token' }, 401
+        return decorated
+    
+    def auth_generateJWT(username) -> str:
+        try:
+            payload = {
+                'exp': datetime.datetime.utcnow() + datetime.timedelta(days=0, minutes=30),
+                'iat': datetime.datetime.utcnow(),
+                'sub': username
+            }
+            return jwt.encode(
+                payload,
+                os.environ.get('SECRET_KEY'),
+                algorithm='HS256'
+            )
+        except Exception as e:
+            return e
+        
+    def auth_decodeJWT(token) -> str:
+        try:
+            payload = jwt.decode(token, os.environ.get('SECRET_KEY'), algorithms=['HS256'])
+            return payload['sub']
+        except jwt.ExpiredSignatureError:
+            raise ExpiredToken('Signature expired. Please log in again.')
+        except jwt.InvalidTokenError:
+            raise InvalidToken('Invalid token. Please log in again.')
 
 
-def auth_decodeJWT(token):
-    try:
-        payload = jwt.decode(token, os.environ.get('SECRET_KEY'), algorithms=['HS256'])
-        return payload['sub']
-    except jwt.ExpiredSignatureError:
-        raise ExpiredToken('Signature expired. Please log in again.')
-    except jwt.InvalidTokenError:
-        raise InvalidToken('Invalid token. Please log in again.')
-
-
-def auth_checkInternal(username, password):
-    user = User.query.filter_by(username=username).first()
-    if user:
-        if user.service == "internal" and user.check_password(password):
-            return username
-        service = services[user.service]
-        if not service:
-            raise UnregisteredService(f'Service {user.service} not registered')
-        user = service['service'].getUser(username, password, service['config'])
+    def auth_checkInternal(self, username: str, password: str) -> User | None:
+        user = User.query.filter_by(username=username).first()
         if user:
-            return username
-    return None
+            if user.service == "internal" and user.check_password(password):
+                return user
+            service = self.services[user.service]
+            if not service:
+                raise UnregisteredService(f'Service {user.service} not registered')
+            user = service['service'].getUser(username, password, service['config'])
+            if user:
+                return user
+        return None
 
-def auth_authenticate(username, password):
-    user = auth_checkInternal(username, password)
-    if user:
-        return { 'username': user, 'token': auth_generateJWT(user) }
-    for service in services.values():
-        user = service['service'].getUser(username, password, service['config'])
+    def auth_authenticate(self, username: str, password: str) -> dict:
+        user = self.auth_checkInternal(username, password)
         if user:
-            return { 'username': user, 'service': service['name'], 'token': auth_generateJWT(user) }
-    raise InvalidCredentials('Invalid credentials')
+            return { 'username': user, 'token': self.auth_generateJWT(user) }
+        for service in self.services.values():
+            user = service['service'].getUser(username, password, service['config'])
+            if user:
+                self._user = user
+                return { 'username': user, 'service': service['name'], 'token': self.auth_generateJWT(user.username) }
+        raise InvalidCredentials('Invalid credentials')
+    
+auth = Auth()
+
