1ca61a3fa5d450389d4312af96dabc9d078e58c5
noah1400 <48360809+noah1400@users.noreply.github.com>
2023-10-28 10:41:59 +0200

    authorizing using roles

--- server/backend/smarttracker/app/auth/auth.py
+++ server/backend/smarttracker/app/auth/auth.py
@@ -11,6 +11,7 @@ import os
 
 class Auth():
     
+    _instance = None
 
     services = {
         'keycloak': {
@@ -25,7 +26,13 @@ class Auth():
         }
     }
 
-    _user = None
+    def __new__(cls):
+        if not cls._instance:
+            cls._instance = super(Auth, cls).__new__(cls)
+        return cls._instance
+    
+    def __init__(self):
+        self._user = None
 
     def auth_authenticated(self, f):
         @wraps(f)
@@ -35,6 +42,7 @@ class Auth():
                 if token:
                     token = token.split(' ')[1] # Bearer <token>
                     username = self.auth_decodeJWT(token)
+                    self._user = User.query.filter_by(username=username).first()
                     if username:
                         return f(username, *args, **kwargs)
             except Exception as e:
@@ -81,8 +89,22 @@ class Auth():
         return None
     
     def auth_upsertUser(self, user: User):
-        db.session.add(user)
-        db.session.commit()
+        existing_user = User.query.filter_by(username=user.username).first()
+        if existing_user:
+            existing_user.username = user.username
+            existing_user.email = user.email
+            existing_user.password_hash = user.password_hash
+            existing_user.service = user.service
+            existing_user.role_id = user.role_id
+            existing_user.created_at = user.created_at
+            existing_user.updated_at = user.updated_at
+            db.session.commit()
+            return existing_user
+        else:
+            new_user = User.create_with_role(user.username, user.email, user.password_hash, user.service, 'user')
+            return new_user
+
+
 
     def auth_authenticate(self, username: str, password: str) -> dict:
         user = self.auth_checkInternal(username, password)
@@ -92,9 +114,13 @@ class Auth():
         for service in self.services.values():
             user = service['service'].getUser(username, password, service['config'])
             if user:
-                self._user = user
+                self._user = self.auth_upsertUser(user)
                 return { 'username': user.username, 'service': service['name'], 'token': self.auth_generateJWT(user.username) }
         raise InvalidCredentials('Invalid credentials')
+    
+    @property
+    def user(self) -> User:
+        return self._user
     
 auth = Auth()
 
--- server/backend/smarttracker/app/graphql/routes.py
+++ server/backend/smarttracker/app/graphql/routes.py
@@ -2,13 +2,16 @@ from app.graphql import bp_graphql, schema, type_defs
 from flask import request, jsonify
 from ariadne import graphql_sync
 from ariadne.explorer.playground import PLAYGROUND_HTML
+from app.auth.auth import auth
 
 @bp_graphql.route('/', methods=['GET'])
 def graphql_playground():
     return PLAYGROUND_HTML, 200
 
 @bp_graphql.route('/', methods=['POST'])
-def graphql():
+@auth.auth_authenticated
+def graphql(username):
+    print(auth.user.to_dict())
     data = request.get_json()
     success, result = graphql_sync(
         schema,
