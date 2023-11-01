6132b47c1ecc2c88da4f1f842a99b71da1304a30
Glika <glika.koutsianou@gmail.com>
2023-11-01 22:50:39 +0100

    first test USB detection app
    npm run electron - to run application

---
+++ desktop/usb-con/index.js
@@ -0,0 +1,90 @@
+// Modules to control application life and create native browser window
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const usb = require('usb');
+
+let windows = [];
+
+const webusb = new usb.WebUSB({
+    allowAllDevices: true
+});
+
+const getDeviceName = (device) => {
+    try {
+        if (device.deviceDescriptor.iProduct !== 0) {
+            return device.getStringDescriptor(device.deviceDescriptor.iProduct);
+        } else {
+            return '<no name>';
+        }
+    } catch (error) {
+        return '<no name>';
+    }
+};
+
+const createWindow = () => {
+    // Create the browser window.
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+            preload: path.join(__dirname, 'preload.js')
+        }
+    });
+
+    // and load the index.html of the app.
+    win.loadFile('index.html');
+
+    // Open the DevTools.
+    win.webContents.openDevTools();
+
+    windows.push(win);
+    showDevices(win);
+};
+
+const showDevices = async (win) => {
+    const devices = await webusb.getDevices();
+    const deviceInfo = devices.map(d => {
+        const name = getDeviceName(d);
+        return `${name}\t${d.vendorId}\t${d.productId}\t${d.serialNumber || '<no serial>'}`;
+    });
+    deviceInfo.unshift('Name\tVID\tPID\tSerial\n-------------------------------------');
+
+    if (win) {
+        win.webContents.send('devices', deviceInfo.join('\n'));
+    }
+};
+
+const logConnection = (device, action) => {
+    const deviceName = getDeviceName(device);
+    const deviceInfo = `Name: ${deviceName}, VID: ${device.vendorId}, PID: ${device.productId}, Serial: ${device.serialNumber || '<no serial>'}`;
+    console.log(`USB Device ${action}: ${deviceInfo}`);
+};
+
+app.whenReady().then(() => {
+    webusb.addEventListener('connect', (device) => {
+        logConnection(device, 'connected');
+        showDevices(windows[0]);
+    });
+
+    webusb.addEventListener('disconnect', (device) => {
+        logConnection(device, 'disconnected');
+        showDevices(windows[0]);
+    });
+
+    createWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0) createWindow();
+    });
+});
+
+app.on('window-all-closed', () => {
+    webusb.removeEventListener('connect', showDevices);
+    webusb.removeEventListener('disconnect', showDevices);
+
+    app.quit();
+
+});
+
+// In this file you can include the rest of your app's specific main process
+// code. You can also put them in separate files and require them here.
