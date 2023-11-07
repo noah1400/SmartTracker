0b1cbbb38b804c803c2bfb6d6c257fd5ca3c9ce5
Glika <glika.koutsianou@gmail.com>
2023-11-07 13:56:43 +0100

    deleted custom name function
    replaced with existing usb-modul function to get device list, more compact and more information available

--- desktop/usb-con/index.js
+++ desktop/usb-con/index.js
@@ -2,26 +2,10 @@ const { app, BrowserWindow } = require('electron');
 const path = require('path');
 const usb = require('usb');
 
+
 let windows = [];
 
-const webusb = new usb.WebUSB({
-    allowAllDevices: true
-});
-
-const getDeviceName = (device) => {
-    try {
-        if (device.deviceDescriptor.iProduct !== 0) {
-            return device.getStringDescriptor(device.deviceDescriptor.iProduct);
-        } else {
-            return '<no name>';
-        }
-    } catch (error) {
-        return '<no name>';
-    }
-};
-
 const createWindow = () => {
-
     const win = new BrowserWindow({
         width: 800,
         height: 600,
@@ -31,53 +15,50 @@ const createWindow = () => {
     });
 
     win.loadFile('index.html');
-
     win.webContents.openDevTools();
-
     windows.push(win);
-    showDevices(win);
 };
 
 const showDevices = async (win) => {
-    const devices = await webusb.getDevices();
-    const deviceInfo = devices.map(d => {
-        const name = getDeviceName(d);
-        return `${name}\t${d.vendorId}\t${d.productId}\t${d.serialNumber || '<no serial>'}`;
-    });
-    deviceInfo.unshift('Name\tVID\tPID\tSerial\n-------------------------------------');
+    const devices = usb.getDeviceList(); 
+    const deviceInfo = devices.map((device,index) => {
+        return `Device ID: ${index + 1}
+        Device: ${device.deviceDescriptor.idVendor}:${device.deviceDescriptor.idProduct}
+        Type: ${device.deviceDescriptor.iProduct}
+        Bus: ${device.busNumber}
+        Address: ${device.deviceAddress}
+        Ports: ${device.portNumbers}
+        Manufacturer: ${device.deviceDescriptor.iManufacturer}
+        Serial Number: ${device.deviceDescriptor.iSerialNumber || '<no serial>'} \n`;
+            });
 
     if (win) {
         win.webContents.send('devices', deviceInfo.join('\n'));
     }
 };
 
-const logConnection = (device, action) => {
-    const deviceName = getDeviceName(device);
-    const deviceInfo = `Name: ${deviceName}, VID: ${device.vendorId}, PID: ${device.productId}, Serial: ${device.serialNumber || '<no serial>'}`;
-    console.log(`USB Device ${action}: ${deviceInfo}`);
-};
 
 app.whenReady().then(() => {
-    webusb.addEventListener('connect', (device) => {
-        logConnection(device, 'connected');
-        showDevices(windows[0]);
-    });
-
-    webusb.addEventListener('disconnect', (device) => {
-        logConnection(device, 'disconnected');
-        showDevices(windows[0]);
-    });
-
     createWindow();
+    
+    showDevices(windows[0]);
+
+    usb.on('attach', () => {
+        showDevices(windows[0]);
+    });
+
+    usb.on('detach', () => {
+        showDevices(windows[0]);
+    });
 
     app.on('activate', () => {
         if (BrowserWindow.getAllWindows().length === 0) createWindow();
     });
+    
 });
 
+
 app.on('window-all-closed', () => {
-    webusb.removeEventListener('connect', showDevices);
-    webusb.removeEventListener('disconnect', showDevices);
 
     app.quit();
 
