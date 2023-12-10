112b699864cc9172966de23829614f82bef702f4
Glika <glika.koutsianou@gmail.com>
2023-12-10 09:20:58 +0100

    fixed app component to display in desktop application correctly
    electron process was conflicting, adjust usb data transfer

--- desktop/usb-con/src/App.tsx
+++ desktop/usb-con/src/App.tsx
@@ -1,55 +1,16 @@
-// src/components/App.tsx
-import React, { useState, useEffect } from 'react';
-import { ipcRenderer } from 'electron';
-import { SerialPortManager } from '../electron/deviceManager'; 
+import React, { FC } from 'react';
 
-const App: React.FC = () => {
-  const [serialData, setSerialData] = useState<string[]>([]);
+interface MyComponentProps {
+  name: string;
+}
 
-  useEffect(() => {
-    // Replace 'COM3' with your actual port name
-    const serialPort = new SerialPortManager('COM4');
-
-    return () => {
-      // Cleanup or close the serial port if needed
-    };
-  }, []);
-
-  useEffect(() => {
-    // Handle data received from the main process
-    const handleSerialData = (event: Electron.IpcRendererEvent, data: string) => {
-      setSerialData((prevData) => [...prevData, data]);
-    };
-
-    ipcRenderer.on('serial-data', handleSerialData);
-
-    return () => {
-      // Remove the event listener when the component unmounts
-      ipcRenderer.removeListener('serial-data', handleSerialData);
-    };
-  }, []);
-
+function MyComponent(){
   return (
-    <div className="App">
-      <h1>Electron React Serialport</h1>
-      <table>
-        <thead>
-          <tr>
-            <th>Index</th>
-            <th>Data</th>
-          </tr>
-        </thead>
-        <tbody>
-          {serialData.map((data, index) => (
-            <tr key={index}>
-              <td>{index + 1}</td>
-              <td>{data}</td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
+    <div>
+      <h1>Hello World,</h1>
+      <p>This is a simple TypeScript React component.</p>
     </div>
   );
 };
 
-export default App;
+export default MyComponent;
