a848b5da55c6fb6621fe5ae91ce014f550383158
Glika <glika.koutsianou@gmail.com>
2023-12-25 18:58:30 +0100

    added buttons to edit projects

---
+++ desktop/usb-con/src/renderer/ProjectSettings.tsx
@@ -0,0 +1,96 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import SpeedDial from '@mui/material/SpeedDial';
+import ClearIcon from '@mui/icons-material/Clear';
+import SpeedDialAction from '@mui/material/SpeedDialAction';
+import AddIcon from '@mui/icons-material/Add';
+import DeleteForeverRoundedIcon from '@mui/icons-material/DeleteForeverRounded';
+import EditIcon from '@mui/icons-material/Edit';
+import { useState } from 'react';
+import { Project } from './types';
+import { SpeedDialIcon } from '@mui/material';
+
+interface ProjectOptionsProps {
+  activeProject: Project | null;
+}
+
+const handleEdit = () => {
+  console.log('Edit');
+};
+const handleNewProject = () => {
+  console.log('New Project');
+};
+const handleDelete = () => {
+  console.log('Delete');
+};
+
+export default function ProjectOptions({
+  activeProject,
+}: Readonly<ProjectOptionsProps>) {
+  return (
+    <Box sx={{ height: 320, transform: 'translateZ(0px)', flexGrow: 1 }}>
+      <SpeedDial
+        ariaLabel="Edit Project"
+        sx={{
+          position: 'absolute',
+          bottom: 16,
+          right: 16,
+          '& .MuiFab-root': {
+            backgroundColor: 'white',
+          },
+          '& .MuiFab-root:hover': {
+            backgroundColor: activeProject
+              ? `${activeProject.color}70`
+              : 'defaultColor',
+          },
+        }}
+        icon={
+          <SpeedDialIcon
+            icon={<EditIcon sx={{ color: '#282a2c' }} />}
+            openIcon={
+              <ClearIcon
+                sx={{
+                  color: 'black',
+                  '&:hover': {
+                    color: 'white',
+                  },
+                }}
+              />
+            }
+          />
+        }
+      >
+        <SpeedDialAction
+          icon={<EditIcon sx={{ color: '#282a2c' }} />}
+          onClick={handleEdit}
+          tooltipTitle="Edit"
+          sx={{
+            '& .MuiSvgIcon-root:hover': {
+              color: 'white',
+            },
+          }}
+        />
+        <SpeedDialAction
+          icon={<AddIcon sx={{ color: '#282a2c' }} />}
+          onClick={handleNewProject}
+          tooltipTitle="New"
+          sx={{
+            '& .MuiSvgIcon-root:hover': {
+              color: 'white',
+            },
+          }}
+        />
+        <SpeedDialAction
+          icon={<DeleteForeverRoundedIcon sx={{ color: '#282a2c' }} />}
+          onClick={handleDelete}
+          tooltipTitle="Delete"
+          sx={{
+            '& .MuiSvgIcon-root:hover': {
+              color: 'white',
+            },
+          }}
+        />
+      </SpeedDial>
+    </Box>
+  );
+}
