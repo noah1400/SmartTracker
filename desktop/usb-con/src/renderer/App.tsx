f1b57f76cb2f3b05e500146ed52eb4eab3d76b8a
Glika <glika.koutsianou@gmail.com>
2023-12-29 21:18:00 +0100

    used react-timer-hook libary for the stopwatch
    more simple to use and manage time logs later

--- desktop/usb-con/src/renderer/App.tsx
+++ desktop/usb-con/src/renderer/App.tsx
@@ -8,24 +8,24 @@ import ProjectOptions from './ProjectSettings';
 import { Box, Container, Grid, Paper } from '@mui/material';
 
 export default function App() {
-  const projects: Project[] = [
-    { id: 1, name: 'Pep.Digital', color: '#FF5733' },
-    { id: 2, name: 'HS Esslingen', color: '#33FF57' },
-    { id: 3, name: 'Projekt SWTM', color: '#5733FF' },
-    { id: 4, name: 'Weihnachtsmarkt', color: '#FF33A1' },
-    { id: 5, name: '2024', color: '#11F3AF' },
-    { id: 6, name: 'Silverster', color: '#008080' },
-  ];
+  const [projects, setProjects] = useState<Project[]>([
+    { id: 1, name: 'Pep.Digital', color: '#FF5733', totalTime: { hours: 0, minutes: 0, seconds: 0 } },
+    { id: 2, name: 'HS Esslingen', color: '#33FF57', totalTime: { hours: 0, minutes: 0, seconds: 0 } },
+    { id: 3, name: 'Projekt SWTM', color: '#5733FF', totalTime: { hours: 0, minutes: 0, seconds: 0 } },
+    { id: 4, name: 'Weihnachtsmarkt', color: '#FF33A1', totalTime: { hours: 0, minutes: 0, seconds: 0 } },
+    { id: 5, name: '2024', color: '#11F3AF', totalTime: { hours: 0, minutes: 0, seconds: 0 } },
+    { id: 6, name: 'Silverster', color: '#008080', totalTime: { hours: 0, minutes: 0, seconds: 0 } },
+  ]);
 
   const [activeProject, setActiveProject] = useState<Project | null>(null);
   const [timeRecords, setTimeRecords] = useState({});
 
-  const handleTimerToggle = (time: any) => {
+  const handleTimerToggle = (time: { hours: number; minutes: number; seconds: number }) => {
     if (activeProject) {
-      //setTimeRecords({...timeRecords, [activeProject.id]: time});
-      console.log('time: ', time, 'spend on project: ', activeProject.name);
+      console.log('time: ', time, 'spent on project: ', activeProject.name);
     }
   };
+  
 
   return (
     <Container maxWidth="lg">
@@ -62,8 +62,8 @@ export default function App() {
               }}
             >
               <Timer
-                onTimeToggle={handleTimerToggle}
                 activeProject={activeProject}
+                onTimeToggle={handleTimerToggle}
               />
             </Box>
             <Box style={{ position: 'absolute', bottom: 0, left: 0, padding: '10px' }}>
--- desktop/usb-con/src/renderer/Timer.tsx
+++ desktop/usb-con/src/renderer/Timer.tsx
@@ -1,104 +1,82 @@
 import React, { useState, useEffect } from 'react';
+import { useStopwatch } from 'react-timer-hook';
 import './Timer.css';
 import { IconButton } from '@mui/material';
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
 import PauseCircleIcon from '@mui/icons-material/PauseCircle';
 import StopCircleIcon from '@mui/icons-material/StopCircle';
-import { act } from 'react-test-renderer';
+
+interface Project {
+  id: number;
+  name: string;
+  color: string;
+  totalTime: { hours: number; minutes: number; seconds: number };
+}
+
+interface TimeLog {
+  projectId: string;
+  startTime: Date;
+  endTime: Date;
+}
 
 interface TimerProps {
-  onTimeToggle: (time: {
-    hours: number;
-    minutes: number;
-    seconds: number;
-  }) => void;
-  activeProject: any;
+  activeProject: Project | null;
+  onTimeToggle: (time: { hours: number; minutes: number; seconds: number }) => void;
+
 }
-const Timer: React.FC<TimerProps> = ({ onTimeToggle, activeProject }) => {
-  const [time, setTime] = useState({ hours: 0, minutes: 0, seconds: 0 });
-  const [isRunning, setIsRunning] = useState(false);
-  const [startTime, setStartTime] = useState<number | null>(null);
-  const [totalElapsedSeconds, setTotalElapsedSeconds] = useState(0); //track total
-  const [lastActiveProj, setLastActiveProj] = useState(activeProject);
-  const [prevProj, setPrevProj] = useState(null);
+
+const Timer: React.FC<TimerProps> = ({ activeProject, onTimeToggle }) => {
+  const { seconds, minutes, hours, isRunning, start, pause, reset } = useStopwatch({ autoStart: false });
+  const [timeLogs, setTimeLogs] = useState<TimeLog[]>(() => {
+    const savedLogs = localStorage.getItem('timeLogs');
+    return savedLogs ? JSON.parse(savedLogs) : [];
+  });
 
   useEffect(() => {
-    let intervalId: any;
-    if (isRunning && activeProject) {
-      intervalId = setInterval(() => {
-        setTotalElapsedSeconds((prevTotal) => prevTotal + 1);
-        const hours = Math.floor(totalElapsedSeconds / 3600);
-        const minutes = Math.floor((totalElapsedSeconds % 3600) / 60);
-        const seconds = totalElapsedSeconds % 60;
-
-        setTime({ hours, minutes, seconds });
-      }, 1000);
-    }
-
-    return () => clearInterval(intervalId);
-  }, [isRunning, activeProject, totalElapsedSeconds]);
+    localStorage.setItem('timeLogs', JSON.stringify(timeLogs));
+  }, [timeLogs]);
 
   useEffect(() => {
-    if (activeProject !== lastActiveProj) {
-      if (totalElapsedSeconds > 0) {
-        onTimeToggle(time);
-      }
-      resetTimer();
-      setLastActiveProj(activeProject);
+    if (activeProject) {
+      resetStopwatch();
     }
   }, [activeProject]);
 
   const toggleTimer = () => {
     if (!isRunning) {
-      setLastActiveProj(activeProject);
+      start();
+      //timeLogs.push({ projectId: activeProject ? activeProject.id : '', startTime: new Date(), endTime: new Date() });
+    } else {
+      pause();
+      onTimeToggle({ hours, minutes, seconds });
     }
-    setIsRunning(!isRunning);
   };
 
-  const resetTimer = () => {
-    onTimeToggle(time);
-    setTime({ hours: 0, minutes: 0, seconds: 0 });
-    setTotalElapsedSeconds(0);
-    setIsRunning(false);
-    setStartTime(null);
-    console.log('reset done');
+  const resetStopwatch = () => {
+    reset(new Date(0), false);
   };
-  const formatTime = (timeObject: {
-    hours: number;
-    minutes: number;
-    seconds: number;
-  }) => {
-    const { hours, minutes, seconds } = timeObject;
-    return `${String(hours).padStart(2, '0')} : ${String(minutes).padStart(
-      2,
-      '0',
-    )} : ${String(seconds).padStart(2, '0')}`;
-  };
+
+  const formatTime = () => `${String(hours).padStart(2, '0')} : ${String(minutes).padStart(2, '0')} : ${String(seconds).padStart(2, '0')}`;
 
   const iconButtonStyle = {
     color: activeProject ? activeProject.color : 'white',
   };
   const isButtonDisabled = !activeProject;
-  const disabledButtonStyle = isButtonDisabled ? { color: 'grey', cursor: 'no active project' } : {};
 
   return (
     <div className="timer-container">
-      <p className="timer-display">{formatTime(time)}</p>
+      <p className="timer-display">{formatTime()}</p>
       <IconButton
         onClick={toggleTimer}
-        style={{ ...iconButtonStyle, ...disabledButtonStyle }}
-        disabled={!activeProject}
+        style={iconButtonStyle}
+        disabled={isButtonDisabled}
       >
-        {isRunning ? (
-          <PauseCircleIcon fontSize="large" />
-        ) : (
-          <PlayCircleIcon fontSize="large" />
-        )}
+        {isRunning ? <PauseCircleIcon fontSize="large" /> : <PlayCircleIcon fontSize="large" />}
       </IconButton>
       <IconButton
-        onClick={resetTimer}
-        style={{...iconButtonStyle, ...disabledButtonStyle }}
-        disabled={!activeProject}
+        onClick={resetStopwatch}
+        style={iconButtonStyle}
+        disabled={isButtonDisabled}
       >
         <StopCircleIcon fontSize="large" />
       </IconButton>
