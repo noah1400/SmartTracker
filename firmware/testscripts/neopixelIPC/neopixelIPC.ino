10f249765a2847f40bdb72f5e2ca789be8111179
Marc <pansmarcjoe@t-online.de>
2023-12-31 22:01:51 +0100

    init testscript

---
+++ firmware/testscripts/neopixelIPC/neopixelIPC.ino
@@ -0,0 +1,31 @@
+#include <Adafruit_NeoPixel.h>
+
+#define PIN            6
+#define NUMPIXELS      4
+
+Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
+
+void setup() {
+  Serial.begin(9600);
+  pixels.begin();
+}
+
+void loop() {
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n');
+    if (input.startsWith("rgb(") && input.endsWith(")")) {
+      input.remove(0, 4);
+      input.remove(input.length() - 1);
+      int comma1 = input.indexOf(',');
+      int comma2 = input.lastIndexOf(',');
+      int red = input.substring(0, comma1).toInt();
+      int green = input.substring(comma1 + 1, comma2).toInt();
+      int blue = input.substring(comma2 + 1).toInt();
+
+      for (int i = 0; i < NUMPIXELS; i++) {
+        pixels.setPixelColor(i, pixels.Color(red, green, blue));
+      }
+      pixels.show();
+    }
+  }
+}
