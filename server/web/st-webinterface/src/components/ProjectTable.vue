d7cfba2ff8345be7982560f4c3d246ef36ea1837
Noah Scholz <48360809+noah1400@users.noreply.github.com>
2024-01-07 12:50:52 +0100

    add descriptions to tables

--- server/web/st-webinterface/src/components/ProjectTable.vue
+++ server/web/st-webinterface/src/components/ProjectTable.vue
@@ -3,7 +3,7 @@
         <div class="mt-8 flow-root">
             <div class="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
                 <div class="inline-block min-w-full py-2 align-middle">
-                    <table class="min-w-full divide-y divide-gray-300">
+                    <table class="min-w-full divide-y divide-gray-300" description="Projects Table">
                         <thead>
                             <tr>
                                 <th scope="col"
